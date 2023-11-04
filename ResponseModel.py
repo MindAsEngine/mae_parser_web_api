2ba39f29619f6cf6e495a4d53706a8d0012bc54d
ProdamGaraj <prodgaraj@yandex.ru>
2023-11-04 20:56:34 +0300

    Add queue and some code refactoring

--- ResponseModel.py
+++ ResponseModel.py
@@ -1,4 +1,5 @@
 class ResponseModel:
-    def __init__(self, data_length: int, data: list):
+    def __init__(self, data_length: int, data: list, status_code: str):
         self.data_length = data_length
         self.data = data
+        self.status_code = status_code
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import asyncio
 from datetime import datetime
 import uvicorn
 from fastapi import FastAPI
 from fastapi_sqlalchemy import DBSessionMiddleware, db
+import logging
 
 from schema import Flat as SchemaFlat
 
@@ -15,7 +17,7 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 
 app = FastAPI()
-
+app.fifo_queue = asyncio.Queue()
 # to avoid csrftokenError
 app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
 
@@ -25,23 +27,29 @@ async def get_all(
         page: int = 0,
         limit: int = 100,
         domain: str = "olx"):
-    data = list(db.session
-                .query(ModelFlat)
-                .filter_by(domain=domain, is_active=True)
-                .order_by(ModelFlat.id)
-                .slice(page * limit, (page + 1) * limit))
+    return await app.fifo_queue.put(get_limit_by_domain(page, limit, domain))
 
-    for i in data:
-        print(i)
-
-    return ResponseModel(
-        data_length=len(data),
-        data=data
-    )
 
 
 @app.post("/post_flat")
 async def post_flat(request: list[SchemaFlat]):
+    return await app.fifo_queue.put(save_flats(request))
+
+
+@app.on_event("startup")
+async def start_db():
+    asyncio.create_task(worker())
+
+
+async def worker():
+    logging.log("Worker started")
+    while True:
+        job = await app.fifo_queue.get()
+        logging.log(f"Got a job: (size of remaining queue: {app.fifo_queue.qsize()})")
+        await job()
+
+
+async def save_flats(request: list[SchemaFlat]):
     for flat in request:
         db_flat = ModelFlat(
             external_id=flat.id,
@@ -64,7 +72,24 @@ async def post_flat(request: list[SchemaFlat]):
             db.session.add(db_flat)
         else:
             db.session.merge(db_flat)
-    return db.session.commit()
+    return ResponseModel(status_code="OK")
+
+
+async def get_limit_by_domain(
+        page: int = 0,
+        limit: int = 100,
+        domain: str = "olx"):
+    return ResponseModel(
+        status_code="Ok",
+        data_length=db.session.query(ModelFlat).filter_by(domain=domain).count(),
+        data=list(db.session
+                  .query(ModelFlat)
+                  .filter_by(domain=domain, is_active=True)
+                  .order_by(ModelFlat.id)
+                  .slice(page * limit, (page + 1) * limit)))
+
+
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
