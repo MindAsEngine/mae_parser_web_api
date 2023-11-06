c6b96929ce25516e043a1311d8e2a1189f1d2c37
ProdamGaraj <prodgaraj@yandex.ru>
2023-11-06 16:19:28 +0300

    code cleanup and add logging to file and console

--- ResponseModel.py
+++ ResponseModel.py
@@ -1,5 +1,6 @@
 class ResponseModel:
     def __init__(self, data_length: int, data: list, status_code: str):
+        self.status_code = status_code
         self.data_length = data_length
         self.data = data
-        self.status_code = status_code
+
--- main.py
+++ main.py
@@ -20,13 +20,37 @@ app = FastAPI()
 app.fifo_queue = asyncio.Queue()
 # to avoid csrftokenError
 app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
+# logging.basicConfig(filename=os.environ['LOG_FILE'],
+#                     level=logging.DEBUG)
+# logger = logging.getLogger(__name__)
+
+# Create a logger
+logger = logging.getLogger('alembic')
+logger.setLevel(logging.DEBUG)
+
+# Create a file handler to redirect log output to a file
+file_handler = logging.FileHandler(os.environ['LOG_FILE'])
+file_handler.setLevel(logging.INFO)
+
+# Create a formatter and add it to the file handler
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+# Add the file handler to the logger
+logger.addHandler(file_handler)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(formatter)
+logger.addHandler(consoleHandler)
+
+# Pass the logger to alembic.config.main() as the logger argument
 
 
 @app.get("/get_flats")
 async def get_all(
         page: int = 0,
-        limit: int = 100,
-        domain: str = "olx"):
+        limit: int = 10000,
+        domain: str = "uybor"):
     return ResponseModel(
         status_code="Ok",
         data_length=db.session.query(ModelFlat).filter_by(domain=domain).count(),
@@ -45,22 +69,26 @@ async def post_flats(request: list[SchemaFlat]):
 
 
 @app.get("/get_count")
-async def get_count_by_domain(domain: str = "olx"):
+async def get_count_by_domain(domain: str = ""):
+    if domain == "":
+        db_power = db.session.query(ModelFlat).count()
+    else:
+        db_power = db.session.query(ModelFlat).filter_by(domain=domain).count()
+    logger.info(f'Total domain:{domain} length: {db_power}')
     return ResponseModel(status_code="OK",
-                         data_length=db.session.query(ModelFlat).filter_by(domain=domain).count(),
+                         data_length=db_power,
                          data=[])
 
 
 @app.on_event("startup")
 async def start_db():
-    print("I am alive!")
     asyncio.create_task(worker())
 
 
 async def worker():
     while True:
         flat = await app.fifo_queue.get()
-        print(f"Processing {flat}")
+        logger.info(f"Processing id: {flat.id} with domain: {flat.domain}")
         await save_flat(flat)
 
 
@@ -85,9 +113,12 @@ async def save_flat(flat: SchemaFlat):
     with db():
         if db.session.query(ModelFlat).filter_by(external_id=flat.id).count() == 0:
             db.session.add(db_flat)
+            logger.info(f'Saving entity:{db_flat.external_id} ADDED')
         else:
             db.session.merge(db_flat)
+            logger.info(f'Updating entity: {db_flat.external_id} MERGED')
         db.session.commit()
+        logger.info(f'OK! Committed total: {db.session.query(ModelFlat).count()}')
     pass
 
 
